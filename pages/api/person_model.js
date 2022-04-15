4e9977f16978a4c4bd3fa7881b1cd78e99f2b1f6
Wimull <aninhasilva@tutanota.com>
2022-04-15 15:07:50 -0300

    Setup of basic REST API endpoint

--- pages/api/person_model.js
+++ pages/api/person_model.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
 const { Schema } = mongoose;
 
 const personSchema = new Schema({
@@ -6,11 +7,16 @@ const personSchema = new Schema({
 	sobrenome: { type: String, required: true },
 	nacionalidade: { type: String, required: true },
 	cep: { type: String, required: true },
+	cpf: { type: String, required: true, unique: true },
 	estado: { type: String, required: true },
 	cidade: { type: String, required: true },
 	logradouro: { type: String, required: true },
 	email: { type: String, required: true },
 	telefone: { type: String, required: true },
+});
+mongoose.connect(process.env.URI, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
 });
 
 module.exports =
